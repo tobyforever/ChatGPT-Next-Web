23a582843839b30fe0b995f821746897a43957d9
linmingzhe <87673112@qq.com>
2024-05-15 11:48:45 +0800

    fix list

--- app/constant.ts
+++ app/constant.ts
@@ -154,14 +154,14 @@ const openaiModels = [
 
 const googleModels = [
   // "gemini-1.0-pro",
-  // "gemini-1.5-pro-latest",
+  "gemini-1.5-pro-latest",
   // "gemini-pro-vision",
 ];
 
 const anthropicModels = [
   // "claude-instant-1.2",
   // "claude-2.0",
-  // "claude-2.1",
+  "claude-2.1",
   // "claude-3-sonnet-20240229",
   // "claude-3-opus-20240229",
   // "claude-3-haiku-20240307",
